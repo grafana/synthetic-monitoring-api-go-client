4f74b4a1c43306b5986533eb4a3c4bfe508e57c6
Marcelo E. Magallon <marcelo.magallon@grafana.com>
2022-09-20 16:57:45 -0600

    Clean up getTenantCommands
    Signed-off-by: Marcelo E. Magallon <marcelo.magallon@grafana.com>

--- cmd/sm-client/tenant.go
+++ cmd/sm-client/tenant.go
@@ -13,61 +13,14 @@ import (
 func getTenantCommands() cli.Commands {
 	return cli.Commands{
 		&cli.Command{
-			Name:  "get-api-token",
-			Usage: "get new Synthetic Monitoring API token",
-			Action: func(c *cli.Context) error {
-				smClient := smapi.NewClient(c.String("sm-api-url"), "", nil)
-
-				var newToken string
-
-				token := c.String("sm-api-token")
-				if token == "" {
-					// We don't have a token, get one by calling the Install path.
-					resp, err := smClient.Install(
-						c.Context,
-						c.Int64("grafana-instance-id"),
-						c.Int64("metrics-instance-id"),
-						c.Int64("logs-instance-id"),
-						c.String("publisher-token"),
-					)
-					if err != nil {
-						return fmt.Errorf("setting up Synthetic Monitoring tenant: %w", err)
-					}
-
-					newToken = resp.AccessToken
-				} else {
-					// We already have a token, get another one by calling the Create path.
-					resp, err := smClient.CreateToken(c.Context)
-					if err != nil {
-						return fmt.Errorf("getting new Synthetic Monitoring tenant token: %w", err)
-					}
-
-					newToken = resp
-				}
-
-				fmt.Printf("token: %s\n", newToken)
-
-				return nil
-			},
+			Name:   "get-api-token",
+			Usage:  "get new Synthetic Monitoring API token",
+			Action: getApiToken,
 		},
 		&cli.Command{
-			Name:  "delete-api-token",
-			Usage: "delete Synthetic Monitoring API token",
-			Action: func(c *cli.Context) error {
-				token := c.String("sm-api-token")
-				if token == "" {
-					return cli.Exit("invalid API token", 1)
-				}
-
-				smClient := smapi.NewClient(c.String("sm-api-url"), token, nil)
-
-				err := smClient.DeleteToken(c.Context)
-				if err != nil {
-					return fmt.Errorf("deleting token: %w", err)
-				}
-
-				return nil
-			},
+			Name:   "delete-api-token",
+			Usage:  "delete Synthetic Monitoring API token",
+			Action: deleteApiToken,
 		},
 		&cli.Command{
 			Name:   "get",
@@ -75,37 +28,62 @@ func getTenantCommands() cli.Commands {
 			Action: getTenant,
 		},
 		&cli.Command{
-			Name:  "update",
-			Usage: "update Synthetic Monitoring tenant",
-			Action: func(c *cli.Context) error {
-				smClient, cleanup, err := newClient(c)
-				if err != nil {
-					return err
-				}
-
-				defer func() { _ = cleanup(c.Context) }()
-
-				var in synthetic_monitoring.Tenant
-				if err := readJsonArg(c.Args().First(), &in); err != nil {
-					return err
-				}
-
-				out, err := smClient.UpdateTenant(c.Context, in)
-				if err != nil {
-					return fmt.Errorf("updating tenant: %w", err)
-				}
-
-				buf, err := json.Marshal(out)
-				if err != nil {
-					return fmt.Errorf("marshaling tenant: %w", err)
-				}
-
-				fmt.Printf("%s\n", string(buf))
-
-				return nil
-			},
+			Name:   "update",
+			Usage:  "update Synthetic Monitoring tenant",
+			Action: updateTenant,
 		},
 	}
+}
+
+func getApiToken(c *cli.Context) error {
+	smClient := smapi.NewClient(c.String("sm-api-url"), "", nil)
+
+	var newToken string
+
+	token := c.String("sm-api-token")
+	if token == "" {
+		// We don't have a token, get one by calling the Install path.
+		resp, err := smClient.Install(
+			c.Context,
+			c.Int64("grafana-instance-id"),
+			c.Int64("metrics-instance-id"),
+			c.Int64("logs-instance-id"),
+			c.String("publisher-token"),
+		)
+		if err != nil {
+			return fmt.Errorf("setting up Synthetic Monitoring tenant: %w", err)
+		}
+
+		newToken = resp.AccessToken
+	} else {
+		// We already have a token, get another one by calling the Create path.
+		resp, err := smClient.CreateToken(c.Context)
+		if err != nil {
+			return fmt.Errorf("getting new Synthetic Monitoring tenant token: %w", err)
+		}
+
+		newToken = resp
+	}
+
+	fmt.Printf("token: %s\n", newToken)
+
+	return nil
+}
+
+func deleteApiToken(c *cli.Context) error {
+	token := c.String("sm-api-token")
+	if token == "" {
+		return cli.Exit("invalid API token", 1)
+	}
+
+	smClient := smapi.NewClient(c.String("sm-api-url"), token, nil)
+
+	err := smClient.DeleteToken(c.Context)
+	if err != nil {
+		return fmt.Errorf("deleting token: %w", err)
+	}
+
+	return nil
 }
 
 func getTenant(c *cli.Context) error {
@@ -142,6 +120,34 @@ func getTenant(c *cli.Context) error {
 	if err := w.Flush(); err != nil {
 		return fmt.Errorf("flushing output: %w", err)
 	}
+
+	return nil
+}
+
+func updateTenant(c *cli.Context) error {
+	smClient, cleanup, err := newClient(c)
+	if err != nil {
+		return err
+	}
+
+	defer func() { _ = cleanup(c.Context) }()
+
+	var in synthetic_monitoring.Tenant
+	if err := readJsonArg(c.Args().First(), &in); err != nil {
+		return err
+	}
+
+	out, err := smClient.UpdateTenant(c.Context, in)
+	if err != nil {
+		return fmt.Errorf("updating tenant: %w", err)
+	}
+
+	buf, err := json.Marshal(out)
+	if err != nil {
+		return fmt.Errorf("marshaling tenant: %w", err)
+	}
+
+	fmt.Printf("%s\n", string(buf))
 
 	return nil
 }
